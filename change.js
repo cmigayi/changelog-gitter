d33a6c556a4ab04723b96cc9c815381f535bbeac
cecil <migayicecil@gmail.com>
2020-10-21 09:50:14 +0300

    args[2]

--- change.js
+++ change.js
@@ -4,26 +4,37 @@ const args = process.argv;
 
 console.log("comment: ",args[2]);
 var commit = "git commit -m args[2]";
-exec("git add .", (error, stdout, stderr) => {
-  if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-  }
-  if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-  }
-  console.log(`stdout: ${stdout}`);
+gitChange();
 
-  exec(commit, (error, stdout, stderr) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-      }
-      console.log(`stdout: ${stdout}`);
-  });
-});
+async function gitChange(){
+  try{
+    await exec("git add .");
+    let result = await exec(commit);
+    console.log("result:", result);
+  }catch(error){
+    console.log("Error:", error);
+  }
+}
+// exec("git add .", (error, stdout, stderr) => {
+//   if (error) {
+//       console.log(`error: ${error.message}`);
+//       return;
+//   }
+//   if (stderr) {
+//       console.log(`stderr: ${stderr}`);
+//       return;
+//   }
+//   console.log(`stdout: ${stdout}`);
+//
+//   exec(commit, (error, stdout, stderr) => {
+//       if (error) {
+//           console.log(`error: ${error.message}`);
+//           return;
+//       }
+//       if (stderr) {
+//           console.log(`stderr: ${stderr}`);
+//           return;
+//       }
+//       console.log(`stdout: ${stdout}`);
+//   });
+// });
