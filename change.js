12742997f390622a9438e06c1de0fd228d38ec3d
cecil <migayicecil@gmail.com>
2020-10-21 10:03:56 +0300

    test

--- change.js
+++ change.js
@@ -3,38 +3,15 @@ const {exec} = require('child_process');
 const args = process.argv;
 
 console.log("comment: ",args[2]);
-var commit = "git commit -m args[2]";
+var commit = "git commit -m test";
 gitChange();
 
 async function gitChange(){
   try{
     await exec("git add .");
     let result = await exec(commit);
-    console.log("result:", result);
+    console.log("Git change successful");
   }catch(error){
     console.log("Error:", error);
   }
 }
-// exec("git add .", (error, stdout, stderr) => {
-//   if (error) {
-//       console.log(`error: ${error.message}`);
-//       return;
-//   }
-//   if (stderr) {
-//       console.log(`stderr: ${stderr}`);
-//       return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-//
-//   exec(commit, (error, stdout, stderr) => {
-//       if (error) {
-//           console.log(`error: ${error.message}`);
-//           return;
-//       }
-//       if (stderr) {
-//           console.log(`stderr: ${stderr}`);
-//           return;
-//       }
-//       console.log(`stdout: ${stdout}`);
-//   });
-// });
