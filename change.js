03f7b214166ea1b4cd90240ea8e476d73d53ef88
cecil <migayicecil@gmail.com>
2020-10-21 09:32:26 +0300

    args[2]

---
+++ change.js
@@ -0,0 +1,18 @@
+const {exec} = require('child_process');
+
+const args = process.argv;
+
+console.log("comment: ",args[2]);
+var commit = "git commit -m args[2]";
+
+exec(commit, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
