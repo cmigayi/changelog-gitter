0a35f45e174dcf1f0512822c39da0389c0d4b7b7
cecil <migayicecil@gmail.com>
2020-10-21 09:43:25 +0300

    args[2]

--- change.js
+++ change.js
@@ -4,15 +4,26 @@ const args = process.argv;
 
 console.log("comment: ",args[2]);
 var commit = "git commit -m args[2]";
+exec("git add .", (error, stdout, stderr) => {
+  if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+  }
+  if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+  }
+  console.log(`stdout: ${stdout}`);
 
-exec(commit, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+  exec(commit, (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+      }
+      console.log(`stdout: ${stdout}`);
+  });
 });
